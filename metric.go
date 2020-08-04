34760d785f5bedacaac6df3c83f676d5d40cd5f6
Malcolm Jones <malcolm.jones@nytimes.com>
2020-08-03 21:15:56 -0700

    metrics is big broken at the moment

--- metric.go
+++ metric.go
@@ -1,55 +1,55 @@
-package datadog
+// package datadog
 
-import (
-	"context"
-	"log"
-	"time"
+// import (
+// 	"context"
+// 	"log"
+// 	"time"
 
-	"go.opentelemetry.io/otel/api/global"
-	export "go.opentelemetry.io/otel/sdk/export/metric"
-	"go.opentelemetry.io/otel/sdk/metric/controller/push"
-	integrator "go.opentelemetry.io/otel/sdk/metric/integrator/simple"
-	"go.opentelemetry.io/otel/sdk/metric/selector/simple"
-)
+// 	"go.opentelemetry.io/otel/api/global"
+// 	export "go.opentelemetry.io/otel/sdk/export/metric"
+// 	"go.opentelemetry.io/otel/sdk/metric/controller/push"
+// 	integrator "go.opentelemetry.io/otel/sdk/metric/integrator/simple"
+// 	"go.opentelemetry.io/otel/sdk/metric/selector/simple"
+// )
 
-func InstallNewPipeline() (*push.Controller, error) {
-	controller, err := NewExportPipeline()
-	if err != nil {
-		return controller, err
-	}
+// func InstallNewPipeline() (*push.Controller, error) {
+// 	controller, err := NewExportPipeline()
+// 	if err != nil {
+// 		return controller, err
+// 	}
 
-	global.SetMeterProvider(controller)
+// 	global.SetMeterProvider(controller)
 
-	return controller, err
-}
+// 	return controller, err
+// }
 
-func NewExportPipeline() (*push.Controller, error) {
-	exp, err := NewMeterExpoter()
-	if err != nil {
-		return nil, err
-	}
+// func NewExportPipeline() (*push.Controller, error) {
+// 	exp, err := NewMeterExpoter()
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	selector := simple.NewWithExactMeasure()
+// 	selector := simple.NewWithExactMeasure()
 
-	integrator := integrator.New(selector, export.NewDefaultLabelEncoder(), true)
+// 	integrator := integrator.New(selector, export.NewDefaultLabelEncoder(), true)
 
-	pusher := push.New(integrator, exp, time.Minute)
-	pusher.Start()
+// 	pusher := push.New(integrator, exp, time.Minute)
+// 	pusher.Start()
 
-	return pusher, nil
-}
+// 	return pusher, nil
+// }
 
-// MeterExporter exports metrics to DataDog
-type MeterExporter struct {
-}
+// // MeterExporter exports metrics to DataDog
+// type MeterExporter struct {
+// }
 
-// NewMeterExpoter constructs a NewMeterExpoter
-func NewMeterExpoter() (*MeterExporter, error) {
-	return &MeterExporter{}, nil
-}
+// // NewMeterExpoter constructs a NewMeterExpoter
+// func NewMeterExpoter() (*MeterExporter, error) {
+// 	return &MeterExporter{}, nil
+// }
 
-// Export exports the provide metric record to DataDog.
-func (e *MeterExporter) Export(ctx context.Context, checkpoint export.CheckpointSet) error {
-	log.Printf("Export Checkpoint: %+v\n", checkpoint)
-	return nil
-}
+// // Export exports the provide metric record to DataDog.
+// func (e *MeterExporter) Export(ctx context.Context, checkpoint export.CheckpointSet) error {
+// 	log.Printf("Export Checkpoint: %+v\n", checkpoint)
+// 	return nil
+// }
